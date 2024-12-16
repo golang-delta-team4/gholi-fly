0c9188da82f2c9ebf41d78a4cfbf2987f445983b
Sepehr <sphr.ghfri@gmail.com>
2024-12-16 20:32:33 +0330

    init sample service

---
+++ micro-services/hotel/cmd/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"flag"
+	"os"
+
+	"gholi-fly-hotel/config"
+)
+
+var configPath = flag.String("config", "config.json", "service configuration file")
+
+func main() {
+	flag.Parse()
+
+	if v := os.Getenv("CONFIG_PATH"); len(v) > 0 {
+		*configPath = v
+	}
+
+	c := config.MustReadConfig(*configPath)
+
+	_ = c
+
+}
