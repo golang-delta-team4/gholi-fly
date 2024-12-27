1ab6d3492c91ab6ac4531a88dd3fbf23215cbc19
sanosrati <saanosrati@gmail.com>
2024-12-27 17:56:14 +0330

    feat(role): add service to create super admin role

--- micro-services/user/internal/role/port/service.go
+++ micro-services/user/internal/role/port/service.go
@@ -10,6 +10,7 @@ import (
 
 type Service interface {
 	CreateRole(ctx context.Context, role *domain.Role) (uuid.UUID, error)
+	CreateSuperAdminRole(ctx context.Context) (error)
 	AssignRole(ctx context.Context, userUUID uuid.UUID, roles []domain.Role) error
 	GrantResourceAccess(ctx context.Context, ownerUUID uuid.UUID, permissions []permissionDomain.Permission, roleName string) error
 }
