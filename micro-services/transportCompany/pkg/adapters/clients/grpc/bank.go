fecea3a28ee391c4ff52ff89b6a36ba4a11f85a7
shahsavari <mohammadnew1377@gmail.com>
2024-12-27 15:15:30 +0330

    feat : price of agency ticket added

--- micro-services/transportCompany/pkg/adapters/clients/grpc/bank.go
+++ micro-services/transportCompany/pkg/adapters/clients/grpc/bank.go
@@ -8,6 +8,7 @@ import (
 	bankClientPort "github.com/golang-delta-team4/gholi-fly/transportCompany/pkg/adapters/clients/grpc/port"
 
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
 type GRPCBankClient struct {
@@ -19,37 +20,8 @@ func NewGRPCBankClient(host string, port int) bankClientPort.GRPCBankClient {
 	return &GRPCBankClient{host: host, port: port}
 }
 
-func (g *GRPCBankClient) CreateUserWallet(userUUID string) (*pb.CreateWalletResponse, error) {
-
-	conn, err := grpc.Dial(fmt.Sprintf("%v:%v", g.host, g.port))
-	if err != nil {
-		return nil, err
-	}
-
-	defer conn.Close()
-
-	// Create a new AuthService client
-	client := pb.NewWalletServiceClient(conn)
-
-	// Create a context
-	ctx := context.Background()
-
-	// Prepare the request
-	request := &pb.CreateWalletRequest{
-		OwnerId: userUUID,
-		Type:    pb.WalletType_PERSON,
-	}
-
-	// Call the GetUserByToken method
-	response, err := client.CreateWallet(ctx, request)
-	if err != nil {
-		return nil, err
-	}
-	return response, nil
-}
-
 func (g *GRPCBankClient) CreateFactor(req *pb.CreateFactorRequest) (*pb.CreateFactorResponse, error) {
-	conn, err := grpc.Dial(fmt.Sprintf("%v:%v", g.host, g.port))
+	conn, err := grpc.Dial(fmt.Sprintf("%v:%v", g.host, g.port), grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
 		return nil, err
 	}
