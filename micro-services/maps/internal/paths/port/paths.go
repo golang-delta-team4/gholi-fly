d201afda3eedc9970173398a6dbe6f11c340d6b3
hesamhme <hessamhashemi73@gmail.com>
2024-12-27 22:10:22 +0330

    feat: add filter path

--- micro-services/maps/internal/paths/port/paths.go
+++ micro-services/maps/internal/paths/port/paths.go
@@ -8,7 +8,8 @@ import (
 type PathRepository interface {
 	GetAll(ctx context.Context) ([]domain.Path, error)
 	Create(ctx context.Context, path *domain.Path) error
-	GetByID(ctx context.Context, id string) (*domain.Path, error) 
-	Update(ctx context.Context, path *domain.Path) error  
-	Delete(ctx context.Context, id string) error 
+	GetByID(ctx context.Context, id string) (*domain.Path, error)
+	Update(ctx context.Context, path *domain.Path) error
+	Delete(ctx context.Context, id string) error
+	FilterPaths(ctx context.Context, filters map[string]interface{}) ([]domain.Path, error) // New method
 }
