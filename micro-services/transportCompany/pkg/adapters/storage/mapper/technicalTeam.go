b1d13c4f40cbd98a9da7f29ac422240ade008fc1
shahsavari <mohammadnew1377@gmail.com>
2024-12-28 13:40:38 +0330

    fix : some bugs fixed

--- micro-services/transportCompany/pkg/adapters/storage/mapper/technicalTeam.go
+++ micro-services/transportCompany/pkg/adapters/storage/mapper/technicalTeam.go
@@ -5,8 +5,8 @@ import (
 	"github.com/golang-delta-team4/gholi-fly/transportCompany/pkg/adapters/storage/types"
 )
 
-func TechnicalTeamDomain2Storage(domain domain.TechnicalTeam) types.TechnicalTeam {
-	return types.TechnicalTeam{
+func TechnicalTeamDomain2Storage(domain domain.TechnicalTeam) *types.TechnicalTeam {
+	return &types.TechnicalTeam{
 		Id:          domain.Id,
 		Name:        domain.Name,
 		Description: domain.Description,
@@ -15,10 +15,30 @@ func TechnicalTeamDomain2Storage(domain domain.TechnicalTeam) types.TechnicalTea
 	}
 }
 
-func TechnicalTeamMemberDomain2Storage(domain domain.TechnicalTeamMember) types.TechnicalTeamMember {
-	return types.TechnicalTeamMember{
+func TechnicalTeamStorage2Domain(storage types.TechnicalTeam) *domain.TechnicalTeam {
+	return &domain.TechnicalTeam{
+		Id:          storage.Id,
+		Name:        storage.Name,
+		Description: storage.Description,
+		CompanyId:   storage.CompanyId,
+		TripType:    storage.TripType,
+		Members:     getIds(storage.Members),
+	}
+}
+
+func TechnicalTeamMemberDomain2Storage(domain domain.TechnicalTeamMember) *types.TechnicalTeamMember {
+	return &types.TechnicalTeamMember{
 		UserId:          domain.UserId,
 		Position:        domain.Position,
 		TechnicalTeamId: domain.TeamId,
 	}
+}
+
+func getIds(members []types.TechnicalTeamMember) []string {
+	result := []string{}
+	for _, item := range members {
+		result = append(result, item.UserId.String())
+	}
+
+	return result
 }
