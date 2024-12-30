4f10215c0210aaa660101d051ceb4d86e488b2bd
shahsavari <mohammadnew1377@gmail.com>
2024-12-30 23:25:18 +0330

    fix : middleware fixed

--- micro-services/transportCompany/api/handlers/http/setup.go
+++ micro-services/transportCompany/api/handlers/http/setup.go
@@ -17,7 +17,6 @@ func Run(appContainer app.App, cfg config.ServerConfig) error {
 	api := router.Group(
 		"/api/v1/transport-company",
 		setUserContext,
-		newAuthMiddleware([]byte(cfg.Secret)),
 	)
 
 	registerCompanyAPI(appContainer, cfg, api)
@@ -30,41 +29,40 @@ func Run(appContainer app.App, cfg config.ServerConfig) error {
 
 func registerCompanyAPI(appContainer app.App, cfg config.ServerConfig, router fiber.Router) {
 	companyServiceGetter := companyServiceGetter(appContainer, cfg)
-	router.Post("/company", setTransaction(appContainer.DB()), CreateCompany(companyServiceGetter))
-	router.Get("/company/:id", setTransaction(appContainer.DB()), GetCompanyById(companyServiceGetter))
-	router.Get("/get-company-by-ownerid/:ownerId", setTransaction(appContainer.DB()), GetByOwnerId(companyServiceGetter))
-	router.Patch("/company/:id", newAuthorizationMiddlewareDirect(appContainer.UserGRPCService()), setTransaction(appContainer.DB()), UpdateCompany(companyServiceGetter))
-	router.Delete("/company/:id", newAuthorizationMiddlewareDirect(appContainer.UserGRPCService()), setTransaction(appContainer.DB()), DeleteCompany(companyServiceGetter))
+
+	router.Post("/company", newAuthMiddleware([]byte(cfg.Secret)), setTransaction(appContainer.DB()), CreateCompany(companyServiceGetter))
+	router.Get("/company/:id", newAuthMiddleware([]byte(cfg.Secret)), setTransaction(appContainer.DB()), GetCompanyById(companyServiceGetter))
+	router.Get("/get-company-by-ownerid/:ownerId", newAuthMiddleware([]byte(cfg.Secret)), setTransaction(appContainer.DB()), GetByOwnerId(companyServiceGetter))
+	router.Patch("/company/:id", newAuthMiddleware([]byte(cfg.Secret)), newAuthorizationMiddlewareDirect(appContainer.UserGRPCService()), setTransaction(appContainer.DB()), UpdateCompany(companyServiceGetter))
+	router.Delete("/company/:id", newAuthMiddleware([]byte(cfg.Secret)), newAuthorizationMiddlewareDirect(appContainer.UserGRPCService()), setTransaction(appContainer.DB()), DeleteCompany(companyServiceGetter))
 }
 
 func registerTripApi(appContainer app.App, cfg config.ServerConfig, router fiber.Router) {
 	tripServiceGetter := tripServiceGetter(appContainer, cfg)
-	router.Post("/trip", setTransaction(appContainer.DB()), CreateTrip(tripServiceGetter, appContainer.UserGRPCService()))
-	router.Get("/trip/:id", setTransaction(appContainer.DB()), GetTripById(tripServiceGetter))
+	router.Post("/trip", newAuthMiddleware([]byte(cfg.Secret)), setTransaction(appContainer.DB()), CreateTrip(tripServiceGetter, appContainer.UserGRPCService()))
+	router.Get("/trip/:id", newAuthMiddleware([]byte(cfg.Secret)), setTransaction(appContainer.DB()), GetTripById(tripServiceGetter))
 	router.Get("/agency-trip/:id", setTransaction(appContainer.DB()), GetAgencyTripById(tripServiceGetter))
-	router.Get("/trip", setTransaction(appContainer.DB()), GetTrips(tripServiceGetter))
-	router.Get("/agency-trip", setTransaction(appContainer.DB()), GetAgencyTrips(tripServiceGetter))
-	router.Patch("/trip/:id", setTransaction(appContainer.DB()), UpdateTrip(tripServiceGetter, appContainer.UserGRPCService()))
-	router.Delete("/trip/:id", setTransaction(appContainer.DB()), DeleteTrip(tripServiceGetter))
+	router.Get("/trip", newAuthMiddleware([]byte(cfg.Secret)), setTransaction(appContainer.DB()), GetTrips(tripServiceGetter))
+	router.Get("/agency-trip", newAuthMiddleware([]byte(cfg.Secret)), setTransaction(appContainer.DB()), GetAgencyTrips(tripServiceGetter))
+	router.Patch("/trip/:id", newAuthMiddleware([]byte(cfg.Secret)), setTransaction(appContainer.DB()), UpdateTrip(tripServiceGetter, appContainer.UserGRPCService()))
+	router.Delete("/trip/:id", newAuthMiddleware([]byte(cfg.Secret)), setTransaction(appContainer.DB()), DeleteTrip(tripServiceGetter))
 
-	// router.Patch("/cancel-trip/:id", setTransaction(appContainer.DB()), CancelTrip(tripServiceGetter))
-	// router.Patch("/finish-trip/:id", setTransaction(appContainer.DB()), FinishTrip(tripServiceGetter))
 	router.Patch("/confirm-trip/:id", setTransaction(appContainer.DB()), ConfirmTrip(tripServiceGetter))
 }
 
 func registerTicketApi(appContainer app.App, cfg config.ServerConfig, router fiber.Router) {
 	ticketServiceGetter := ticketServiceGetter(appContainer, cfg)
-	router.Post("/ticket/buy", setTransaction(appContainer.DB()), BuyTicket(ticketServiceGetter))
+	router.Post("/ticket/buy", newAuthMiddleware([]byte(cfg.Secret)), setTransaction(appContainer.DB()), BuyTicket(ticketServiceGetter))
 	router.Post("/ticket/agency-buy", setTransaction(appContainer.DB()), BuyAgencyTicket(ticketServiceGetter))
-	router.Post("/ticket/cancel/:id", setTransaction(appContainer.DB()), CancelTicket(ticketServiceGetter))
+	router.Post("/ticket/cancel/:id", newAuthMiddleware([]byte(cfg.Secret)), setTransaction(appContainer.DB()), CancelTicket(ticketServiceGetter))
 	router.Post("/agency-ticket/cancel/:id", setTransaction(appContainer.DB()), CancelAgencyTicket(ticketServiceGetter))
 }
 
 func registerTechnicalTeamApi(appContainer app.App, cfg config.ServerConfig, router fiber.Router) {
 	technicalTeamServiceGetter := technicalTeamServiceGetter(appContainer, cfg)
-	router.Post("/technical-team", setTransaction(appContainer.DB()), CreateTechnicalTeam(technicalTeamServiceGetter))
-	router.Post("/technical-team-member", setTransaction(appContainer.DB()), AddTechnicalTeamMember(technicalTeamServiceGetter))
-	router.Get("/technical-team/:id", setTransaction(appContainer.DB()), GetTechnicalTeamById(technicalTeamServiceGetter))
-	router.Get("/technical-team", setTransaction(appContainer.DB()), GetTechnicalTeams(technicalTeamServiceGetter))
-	router.Patch("/set-technical-team/", setTransaction(appContainer.DB()), SetTechTeamToTrip(technicalTeamServiceGetter))
+	router.Post("/technical-team", newAuthMiddleware([]byte(cfg.Secret)), setTransaction(appContainer.DB()), CreateTechnicalTeam(technicalTeamServiceGetter))
+	router.Post("/technical-team-member", newAuthMiddleware([]byte(cfg.Secret)), setTransaction(appContainer.DB()), AddTechnicalTeamMember(technicalTeamServiceGetter))
+	router.Get("/technical-team/:id", newAuthMiddleware([]byte(cfg.Secret)), setTransaction(appContainer.DB()), GetTechnicalTeamById(technicalTeamServiceGetter))
+	router.Get("/technical-team", newAuthMiddleware([]byte(cfg.Secret)), setTransaction(appContainer.DB()), GetTechnicalTeams(technicalTeamServiceGetter))
+	router.Patch("/set-technical-team/", newAuthMiddleware([]byte(cfg.Secret)), setTransaction(appContainer.DB()), SetTechTeamToTrip(technicalTeamServiceGetter))
 }
