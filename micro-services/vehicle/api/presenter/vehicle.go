71e8712264f6ac14300bebe4aa1e7aeb8a39bbf5
sanosrati <saanosrati@gmail.com>
2024-12-31 11:34:39 +0330

    refactor(vehicle): refactor vehicle create api to get owner id from jwt token

--- micro-services/vehicle/api/presenter/vehicle.go
+++ micro-services/vehicle/api/presenter/vehicle.go
@@ -1,8 +1,24 @@
 package presenter
 
 import (
+	"time"
+
 	"github.com/google/uuid"
 )
+
+type Vehicle struct {
+	ID                uuid.UUID   `json:"id"`
+	OwnerID           uuid.UUID   `json:"owner_id"`
+	Type              VehicleType `json:"type"` // e.g., bus, train, plane, ship
+	Capacity          int         `json:"capacity"`
+	Speed             float64     `json:"speed"`
+	UniqueCode        string      `json:"unique_code"`
+	Status            string      `json:"status"`
+	YearOfManufacture int         `json:"year_of_manufacture"`
+	PricePerKilometer float64     `json:"price_per_kilometer"`
+	CreatedAt         time.Time   `json:"created_at"`
+	UpdatedAt         time.Time   `json:"updated_at"`
+}
 
 type MatchMakerRequest struct {
 	TripID             uuid.UUID
@@ -21,4 +37,13 @@ var (
 	GroundTrip TripType = "ground"
 	AirTrip    TripType = "air"
 	SeaTrip    TripType = "sea"
+)
+
+type VehicleType string
+
+var (
+	Bus   VehicleType = "bus"
+	Train VehicleType = "train"
+	Ship  VehicleType = "ship"
+	Plane VehicleType = "plane"
 )
