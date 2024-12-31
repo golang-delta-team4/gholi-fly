d67aff11084d2fdba7764d52591c51ad44020153
sanosrati <saanosrati@gmail.com>
2024-12-31 23:33:47 +0330

    feat: connect create trip to vehicle mather

---
+++ micro-services/transportCompany/pkg/adapters/clients/http/presenter/path.go
@@ -0,0 +1,11 @@
+package presenter
+
+import "github.com/google/uuid"
+
+type GetPathByIDResponse struct {
+	SourceTerminalID      uuid.UUID `json:"source_terminal_id"`
+	DestinationTerminalID uuid.UUID `json:"destination_terminal_id"`
+	DistanceKM            float64   `json:"distance_km"`
+	RouteCode             string    `json:"route_code"`
+	VehicleType           string    `json:"vehicle_type"`
+}
