1f9b570833a9116bdfd48c11da1583654a8d9203
hesamhme <hessamhashemi73@gmail.com>
2024-12-31 19:07:45 +0330

    feat: validation added

--- micro-services/transportCompany/internal/ticket/domain/ticket.go
+++ micro-services/transportCompany/internal/ticket/domain/ticket.go
@@ -1,6 +1,8 @@
 package domain
 
 import (
+	"errors"
+
 	"github.com/google/uuid"
 )
 
@@ -14,4 +16,35 @@ type Ticket struct {
 	Status          string
 	InvoiceId       uuid.UUID
 	OwnerOfAgencyId uuid.UUID
+}
+
+func (t *Ticket) Validate() error {
+	if t.Id == uuid.Nil {
+		return errors.New("ticket ID can't be nil")
+	}
+	if t.TripID == uuid.Nil {
+		return errors.New("trip ID can't be nil")
+	}
+	if t.UserID == nil {
+		return errors.New("user ID can't be nil")
+	}
+	if t.AgencyID == nil {
+		return errors.New("agency ID can't be nil")
+	}
+	if t.Count == 0 {
+		return errors.New("count must be greater than zero")
+	}
+	if t.Price <= 0 {
+		return errors.New("price must be greater than zero")
+	}
+	if t.Status == "" {
+		return errors.New("status can't be empty")
+	}
+	if t.InvoiceId == uuid.Nil {
+		return errors.New("invoice ID can't be nil")
+	}
+	if t.OwnerOfAgencyId == uuid.Nil {
+		return errors.New("owner of agency ID can't be nil")
+	}
+	return nil
 }
