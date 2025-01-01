f7c63da3964c122d436b81e95ab2e511dccbd208
hesamhme <hessamhashemi73@gmail.com>
2025-01-01 14:57:34 +0330

    fix: delete extra comment parts

--- micro-services/maps/api/handlers/http/setup.go
+++ micro-services/maps/api/handlers/http/setup.go
@@ -19,30 +19,3 @@ func SetupRouter(terminalService port_t.TerminalService, pathService port_p.Path
 	return app
 }
 
-// import (
-// 	"net/http"
-
-// 	port_p"gholi-fly-maps/internal/paths/port"
-// 	port_t"gholi-fly-maps/internal/terminals/port"
-
-// 	"github.com/go-chi/chi/v5"
-// 	"github.com/go-chi/chi/v5/middleware"
-// )
-
-// // SetupRouter initializes the HTTP router and registers all routes.
-// func SetupRouter(terminalService port_t.TerminalService, pathService port_p.PathService) http.Handler {
-// 	// Create a new router instance
-// 	r := chi.NewRouter()
-
-// 	// Apply middleware
-// 	r.Use(middleware.Logger)
-// 	r.Use(middleware.Recoverer)
-
-// 	// Register terminal routes
-// 	RegisterTerminalRoutes(r, terminalService)
-
-// 	// Register path routes
-// 	RegisterPathRoutes(r, pathService)
-
-// 	return r
-// }
